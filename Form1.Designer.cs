410af86e5e7cf2be5544b9ac9614023ba980b253
Ulises PG <99633145+ulipg@users.noreply.github.com>
2023-11-13 18:58:35 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ProyectoFinalPOO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDni = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.DataConsulta = new System.Windows.Forms.DataGridView();
+            this.TxtDni = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.TxtDireccion = new System.Windows.Forms.TextBox();
+            this.TxtTelefono = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnRegistar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.TxtCorreo = new System.Windows.Forms.TextBox();
+            this.btnForms2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataConsulta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(24, 48);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(23, 13);
+            this.lblDni.TabIndex = 0;
+            this.lblDni.Text = "Dni";
+            this.lblDni.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(24, 96);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(24, 157);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(52, 13);
+            this.lblDireccion.TabIndex = 2;
+            this.lblDireccion.Text = "Direccion";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(24, 217);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(49, 13);
+            this.lblTelefono.TabIndex = 3;
+            this.lblTelefono.Text = "Telefono";
+            // 
+            // DataConsulta
+            // 
+            this.DataConsulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataConsulta.Location = new System.Drawing.Point(271, 12);
+            this.DataConsulta.Name = "DataConsulta";
+            this.DataConsulta.Size = new System.Drawing.Size(796, 476);
+            this.DataConsulta.TabIndex = 4;
+            // 
+            // TxtDni
+            // 
+            this.TxtDni.Location = new System.Drawing.Point(94, 45);
+            this.TxtDni.Name = "TxtDni";
+            this.TxtDni.Size = new System.Drawing.Size(170, 20);
+            this.TxtDni.TabIndex = 5;
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(94, 93);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(170, 20);
+            this.TxtNombre.TabIndex = 6;
+            // 
+            // TxtDireccion
+            // 
+            this.TxtDireccion.Location = new System.Drawing.Point(95, 150);
+            this.TxtDireccion.Name = "TxtDireccion";
+            this.TxtDireccion.Size = new System.Drawing.Size(170, 20);
+            this.TxtDireccion.TabIndex = 7;
+            // 
+            // TxtTelefono
+            // 
+            this.TxtTelefono.Location = new System.Drawing.Point(95, 214);
+            this.TxtTelefono.Name = "TxtTelefono";
+            this.TxtTelefono.Size = new System.Drawing.Size(170, 20);
+            this.TxtTelefono.TabIndex = 8;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(56, 511);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(140, 45);
+            this.btnInsertar.TabIndex = 9;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(253, 511);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(122, 45);
+            this.btnActualizar.TabIndex = 10;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnRegistar
+            // 
+            this.btnRegistar.Location = new System.Drawing.Point(427, 511);
+            this.btnRegistar.Name = "btnRegistar";
+            this.btnRegistar.Size = new System.Drawing.Size(133, 45);
+            this.btnRegistar.TabIndex = 11;
+            this.btnRegistar.Text = "Registrar";
+            this.btnRegistar.UseVisualStyleBackColor = true;
+            this.btnRegistar.Click += new System.EventHandler(this.btnRegistar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(629, 511);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(123, 45);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Location = new System.Drawing.Point(27, 273);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(38, 13);
+            this.lblCorreo.TabIndex = 13;
+            this.lblCorreo.Text = "Correo";
+            // 
+            // TxtCorreo
+            // 
+            this.TxtCorreo.Location = new System.Drawing.Point(95, 273);
+            this.TxtCorreo.Name = "TxtCorreo";
+            this.TxtCorreo.Size = new System.Drawing.Size(170, 20);
+            this.TxtCorreo.TabIndex = 14;
+            // 
+            // btnForms2
+            // 
+            this.btnForms2.Location = new System.Drawing.Point(862, 511);
+            this.btnForms2.Name = "btnForms2";
+            this.btnForms2.Size = new System.Drawing.Size(123, 45);
+            this.btnForms2.TabIndex = 15;
+            this.btnForms2.Text = "Forms2";
+            this.btnForms2.UseVisualStyleBackColor = true;
+            this.btnForms2.Click += new System.EventHandler(this.btnForms2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 597);
+            this.Controls.Add(this.btnForms2);
+            this.Controls.Add(this.TxtCorreo);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnRegistar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.TxtTelefono);
+            this.Controls.Add(this.TxtDireccion);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.TxtDni);
+            this.Controls.Add(this.DataConsulta);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblDni);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataConsulta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.DataGridView DataConsulta;
+        private System.Windows.Forms.TextBox TxtDni;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.TextBox TxtDireccion;
+        private System.Windows.Forms.TextBox TxtTelefono;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnRegistar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.TextBox TxtCorreo;
+        private System.Windows.Forms.Button btnForms2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinalPOO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Conexion con = new Conexion();
+            con.RegistrarCliente(DataConsulta);
+
+        }
+
+        private void TxtDNI_TextChanged(object sender, EventArgs e)
+        {
+            Conexion con = new Conexion();
+            con.RegistrarCliente(DataConsulta, TxtDni.Text);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistar_Click(object sender, EventArgs e)
+        {
+            Conexion con = new Conexion();
+            MessageBox.Show(con.insertarCliente(TxtDni.Text, TxtNombre.Text, TxtDireccion.Text, TxtTelefono.Text, TxtCorreo.Text));
+            TxtDni.Text = string.Empty;
+            TxtDireccion.Text = string.Empty;
+            TxtNombre.Text = string.Empty;
+            TxtTelefono.Text = string.Empty;
+            TxtCorreo.Text = string.Empty;
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Conexion con = new Conexion();
+            MessageBox.Show(con.eliminarCliente(TxtDni.Text));
+            con.eliminarCliente(con.eliminarCliente(TxtDni.Text));
+            TxtDireccion.Text = string.Empty;
+            TxtNombre.Text = string.Empty;
+            TxtTelefono.Text = string.Empty;
+            TxtCorreo.Text = string.Empty;
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            Conexion con = new Conexion();
+            MessageBox.Show(con.actualizarCliente(TxtDni.Text, TxtNombre.Text, TxtDireccion.Text, TxtTelefono.Text, TxtCorreo.Text));
+            TxtDireccion.Text = string.Empty;
+            TxtNombre.Text = string.Empty;
+            TxtTelefono.Text = string.Empty;
+            TxtCorreo.Text = string.Empty;
+
+        }
+
+
+                
+
+
+
+
+
+                private void btnForms2_Click(object sender, EventArgs e)
+                {
+
+                }
+            
+        
+    }
+}
+
+
